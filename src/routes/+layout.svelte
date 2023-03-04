ac5deb0d09e8ebc76d7e11cb8f69a4bc6c749859
Stephen Fillers <9887645+stephenfillers@users.noreply.github.com>
2023-03-04 13:05:30 -0500

    Introduced barebones layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,68 @@
+<script>
+	import '../styles/global.css';
+</script>
+
+<div class="main-layout">
+	<nav>
+		<a href="/" class="nav-logo">Sentiment Sage</a>
+		<div class="nav-links">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+		</div>
+	</nav>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>Copyright 2023 Sentiment Sage</footer>
+</div>
+
+<style>
+	.main-layout {
+		min-height: 100svh;
+		display: grid;
+		grid-template-rows:
+			auto
+			1fr
+			auto;
+	}
+
+	nav {
+		height: 60px;
+		background-color: #2e1f42;
+		color: #fff;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.nav-logo {
+		font-family: sans-serif;
+		font-size: 1.2rem;
+		margin: 0 1rem;
+		color: #fff;
+		text-decoration: none;
+	}
+
+	.nav-links > a {
+		color: #fff;
+		text-decoration: none;
+		margin: 0 1rem 0 0;
+	}
+
+	main {
+		padding: 1rem;
+	}
+
+	footer {
+		height: 60px;
+		background-color: #2e1f42;
+		color: #fff;
+		display: flex;
+		align-items: center;
+		padding: 0 1rem;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,43 @@
+<h1>About Sentiment Sage.</h1>
+<p>
+	Introducing our cutting-edge software that uses the latest machine learning algorithms to help
+	businesses gather valuable insights about their brand's sentiment on social media.
+</p>
+
+<p>
+	With our software, you can easily track and analyze social media conversations about your brand,
+	allowingyou to stay ahead of the competition and make data-driven decisions. Our software is
+	designed to be user-friendly, intuitive, and accessible to users with varying levels of technical
+	expertise.
+</p>
+
+<p>
+	Our software uses advanced machine learning techniques to analyze social media posts and comments
+	in real-time, providing you with accurate and up-to-date information about your brand's sentiment.
+	You can easily view sentiment analysis results, such as positive, negative, or neutral sentiment,
+	in a clear and visually appealing dashboard.
+</p>
+
+<p>
+	Our software also offers advanced analytics and reporting features that allow you to dive deeper
+	into the data and gain a better understanding of your brand's reputation on social media. You can
+	identify the most popular topics related to your brand, track sentiment changes over time, and
+	even monitor sentiment across different social media platforms.
+</p>
+
+<p>
+	Overall, our software is a powerful tool for businesses looking to gather insights about their
+	brand's sentiment on social media. It is easy to use, accurate, and provides valuable information
+	that can help you make informed decisions and improve your brand's reputation. Try it today and
+	discover the power of machine learning for social media sentiment analysis!
+</p>
+
+<style>
+	h1 {
+		color: #fff;
+	}
+
+	p {
+		color: #fff;
+	}
+</style>
